62ba7099b38fcdc442d000b0a92c34823204bb1d
Oksana Zakharchuk <126189342+OksaZa@users.noreply.github.com>
2023-07-03 10:24:45 +0300

    edit

--- js/task-01.js
+++ js/task-01.js
@@ -3,9 +3,8 @@ const items = categories.children;
 console.log(items.length);
 
 [...items].forEach((item) => {
-  const title = item.querySelector("h2").textContent;
-  const itemList = item.querySelectorAll("li");
-  const itemCount = itemList.length;
+  const title = item.firstElementChild.textContent;
+  const itemCount = item.lastElementChild.children.length;
 
   console.log("Заголовок: ", title);
   console.log("Кількість елементів: ", itemCount);
