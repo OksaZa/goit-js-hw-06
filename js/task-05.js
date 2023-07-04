dd97ff03db83d9bddbe80efb969eeccd313a077a
Oksana Zakharchuk <126189342+OksaZa@users.noreply.github.com>
2023-07-04 08:17:44 +0300

    t5

--- js/task-05.js
+++ js/task-05.js
@@ -3,9 +3,5 @@ const nameOutput = document.querySelector("#name-output");
 
 nameInput.addEventListener("input", onInput);
 function onInput(evt) {
-    if (nameInput.value !== "") {
-        nameOutput.textContent = nameInput.value.trim();
-    } else {
-        nameOutput.textContent = "Anonymous";
-    }
+    nameOutput.textContent = nameInput.value === "" ? "Anonymous" : nameInput.value.trim();
 };
